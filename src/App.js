50fcb4ede6a02a55f2c8fc93c06023d8404eaef3
Cheese <HAhhIMAMuffin@gmail.com>
2024-09-07 17:21:50 +0700

    added MUI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
-import Grid from '@mui/material/Grid2';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+
 import * as React from 'react';
 //import Box from '@mui/material/Box';
 
 
-const LoginScreen = () => {
+const App = () => {
   return (
-    <Grid size={{ xs: 12, md: 5 }}>
-      <TextField id="outlined-basic" label="Email Address" variant="outlined" />
-      <TextField id="outlined-basic" label="Password" variant="outlined" />
-      <Button variant="contained">Login</Button>
+    <Grid container spacing={2} style={{ height: '50vh', width: '50vw', margin: 'auto' }}>
+    <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+      <h1>Login</h1>
+      <form>
+        <TextField
+          id="email"
+          label="Email"
+          type="email"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Login
+        </Button>
+      </form>
     </Grid>
+  </Grid>
   );
 };
 
-export default LoginScreen;
+export default App;
